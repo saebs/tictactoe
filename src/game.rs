967595cb929c60f3d1a80abbc7b0804a15115ffb
sabelo <ntabenism@gmail.com>
2018-08-06 14:19:18 +0200

    player is finished

--- src/game.rs
+++ src/game.rs
@@ -146,3 +146,18 @@ fn game_is_won(&self) -> bool {
 
     (all_same_row || all_same_col || all_same_diag_1 || all_same_diag_2)
 }
+
+// Player is finished method
+
+fn player_is_finished() -> bool {
+    let mut player_input = String::new();
+    println!("Are you finished playing (y/n)?:");
+    match io::stdin().read_line(&mut player_input) {
+        Ok(_) => {
+            let temp = player_input.to_lowercase();
+            temp.trim() == "y" || temp.trim() == "yes"
+        }
+        Err(_) => false, // I suspect a bug from comma
+    }
+
+}
